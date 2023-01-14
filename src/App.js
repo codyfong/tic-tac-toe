cf64c448befb068eeb041e23f9521e4832eb7bfe
Cody <icodyfong@gmail.com>
2023-01-14 08:04:55 -0700

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import Square from "./Square";
+
 
 function App() {
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""])
+  const [player, setPlayer] = useState(true)
+  
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""])
+    setPlayer(true)
+}
+
+  const calculateWinner = (array) => {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+    // lines.forEach(line => {
+    //   const [a,b,c] = line
+    //   if (a && a === b && a === c && b === c) {
+    //     let myVar = array[a]
+    //   }
+    // });
+    // return(`${myVar} won`)
+    for (let i = 0; i < lines.length; i++){
+            const [a,b,c] = lines[i]
+      if (array[a] && array[a] === array[b] && array[a] === array[c] && array[b] === array[c]) {
+        return(`${array[a]} won`)
+      }
+    }
+
+    return('who will win?')
+  }
+  // const propVariable = 'This is a prop'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span> 
+        {calculateWinner(squares)}
+      </span>
+      <button onClick={handleClick}>Reset</button>
+        <div className="container">
+            {squares.map((val, index) => {
+                return (
+                    <Square
+                        setSquares={setSquares}
+                        index={index}
+                        squareValue={val}
+                        squares={squares}
+                        player={player}
+                        setPlayer={setPlayer}
+                    />
+                );
+            })}
+        </div>
     </div>
-  );
+)
 }
 
 export default App;
